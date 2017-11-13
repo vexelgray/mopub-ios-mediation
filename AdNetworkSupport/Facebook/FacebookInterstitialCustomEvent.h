24b9a0ffe28e08acdb8a91293b033ff365981aa2
PoojaChirp <pshashidhar@twitter.com>
2017-11-13 15:16:29 -0500

    Added network adapters
    Added network adapters

---
+++ AdNetworkSupport/Facebook/FacebookInterstitialCustomEvent.h
@@ -0,0 +1,24 @@
+//
+//  FacebookInterstitialCustomEvent.h
+//  MoPub
+//
+//  Copyright (c) 2014 MoPub. All rights reserved.
+//
+
+#if __has_include(<MoPub/MoPub.h>)
+    #import <MoPub/MoPub.h>
+#else
+    #import "MPInterstitialCustomEvent.h"
+    #import "MPRealTimeTimer.h"
+
+#endif
+
+/*
+ * Certified with Facebook Audience Network 4.23
+ */
+@interface FacebookInterstitialCustomEvent : MPInterstitialCustomEvent
+
+
+
+
+@end
